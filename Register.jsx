dd0f11a001d14adb713a23f1f2014fc284d12825
vaibhav9t1 <115015082+vaibhav9t1@users.noreply.github.com>
2023-07-06 23:25:19 +0530

    Create Register.jsx

---
+++ Register.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import Add from "../img/addAvatar.png";
+import './Register.scss';
+
+
+const Register = () => {
+
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Lama Chat</span>
+        <span className="title">Register</span>
+        <form >
+          <input required type="text" placeholder="display name" />
+          <input required type="email" placeholder="email" />
+          <input required type="password" placeholder="password" />
+          <input required style={{ display: "none" }} type="file" id="file" />
+          <label htmlFor="file">
+            <img src={Add} alt="" />
+            <span>Add an avatar</span>
+          </label>
+          <button>Sign up</button>
+          {/* {loading && "Uploading and compressing the image please wait..."} */}
+          {/* {err && <span>Something went wrong</span>} */}
+        </form>
+        <p>
+          {/* You do have an account? <Link to="/register">Login</Link> */}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
